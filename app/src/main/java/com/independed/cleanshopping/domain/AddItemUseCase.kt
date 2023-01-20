c81778add3fac5884c394c1e03bc99d79e0c44c0
stepan <stvbuz@gmail.com>
2023-01-21 00:39:29 +0500

    Adding first useCases for domain layer

---
+++ app/src/main/java/com/independed/cleanshopping/domain/AddItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.independed.cleanshopping.domain
+
+class AddItemUseCase {
+    fun addElement(shopItem: ShopItem){
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/independed/cleanshopping/domain/GetShoppingListUseCase.kt
@@ -0,0 +1,8 @@
+package com.independed.cleanshopping.domain
+
+class GetShoppingListUseCase {
+
+    fun getShoppingList() : List<ShopItem>{
+        TODO()
+    }
+}
